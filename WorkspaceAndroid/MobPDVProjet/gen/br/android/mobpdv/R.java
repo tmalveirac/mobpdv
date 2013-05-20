4aa2e3604cb54f08fdc38c5cf592abdfd07681cc
tmalveirac <tmalveirac@gmail.com>
2013-05-20 18:16:51 -0300

    Tela de Cardápio Iniciada. Funcionalidade de Expansão de produtos por categorias OK.

--- WorkspaceAndroid/MobPDVProjet/gen/br/android/mobpdv/R.java
+++ WorkspaceAndroid/MobPDVProjet/gen/br/android/mobpdv/R.java
@@ -24,19 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000c;
-        public static final int action_settings=0x7f08000b;
-        public static final int btnAbrCxa=0x7f080008;
-        public static final int btnFecCxa=0x7f08000a;
-        public static final int btnVerCdp=0x7f080009;
-        public static final int email=0x7f080003;
-        public static final int login=0x7f080005;
-        public static final int login_form=0x7f080002;
-        public static final int login_status=0x7f080000;
-        public static final int login_status_message=0x7f080001;
-        public static final int password=0x7f080004;
-        public static final int sign_in_button=0x7f080006;
-        public static final int textView1=0x7f080007;
+        public static final int action_forgot_password=0x7f08000d;
+        public static final int action_settings=0x7f08000c;
+        public static final int btnAbrCxa=0x7f080009;
+        public static final int btnFecCxa=0x7f08000b;
+        public static final int btnVerCdp=0x7f08000a;
+        public static final int email=0x7f080004;
+        public static final int login=0x7f080006;
+        public static final int login_form=0x7f080003;
+        public static final int login_status=0x7f080001;
+        public static final int login_status_message=0x7f080002;
+        public static final int lxpCardapio=0x7f080000;
+        public static final int password=0x7f080005;
+        public static final int sign_in_button=0x7f080007;
+        public static final int textView1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_cardapio=0x7f030000;
--- WorkspaceAndroid/MobPDVProjet/src/br/android/mobpdv/view/CardapioActivity.java
+++ WorkspaceAndroid/MobPDVProjet/src/br/android/mobpdv/view/CardapioActivity.java
@@ -2,20 +2,62 @@ package br.android.mobpdv.view;
 
 import br.android.mobpdv.R;
 import android.os.Bundle;
+import android.app.ExpandableListActivity;
 import android.app.ListActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class CardapioActivity extends CicloVidaActivity {
 
+	ExpandableListView list;
+	ExpandableListAdapter adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_cardapio);
+		setContentView(R.layout.activity_cardapio);
 	
+		list = (ExpandableListView) this.findViewById(R.id.lxpCardapio); 
+		
+		adapter = new CategoriaAdapter(this);
+		list.setAdapter(adapter);
+		
+		
+		list.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+			
+			@Override
+			public boolean onGroupClick(ExpandableListView parent, View v,
+					int groupPosition, long id) {
+				return list_onGroupClick(parent, v, groupPosition, id);
+			}
+		});
+		
+		list.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+			
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v,
+					int groupPosition, int childPosition, long id) {
+				return list_onChildClick(parent, v, groupPosition, childPosition, id);
+			}
+		});
+	}
+	
+	public boolean list_onGroupClick(ExpandableListView parent, View v,
+			int groupPosition, long id) {
+		
+		return false;
+		
+	}
+	
+	public boolean list_onChildClick(ExpandableListView parent, View v,
+			int groupPosition, int childPosition, long id) {
+		Toast.makeText(CardapioActivity.this, adapter.getChild(groupPosition, childPosition).toString(), Toast.LENGTH_SHORT).show();
+		return false;
 	}
 	
 
@@ -25,5 +67,7 @@ public class CardapioActivity extends CicloVidaActivity {
 		getMenuInflater().inflate(R.menu.cardapio, menu);
 		return true;
 	}
+	
+	
 
 }
